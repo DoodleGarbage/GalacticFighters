91be359f2f5dc8d12a968edac094286feec98bb6
DoodleGarbage <redstone48rocks@gmail.com>
2026-02-14 23:06:53 -0800

    first commit

---
+++ stat_indicator.gd
@@ -0,0 +1,13 @@
+@tool
+extends Panel
+
+
+@export var image : Texture2D :
+	set(value):
+		image = value
+		$Life/imgStat.texture = image
+
+@export var stat : int = 0 :
+	set(value):
+		stat = value
+		$Life/statText.text = str(stat)
