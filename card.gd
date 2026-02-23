420e71d32a13012bcf88c96bb92f91dd8046adff
DoodleGarbage <redstone48rocks@gmail.com>
2026-02-22 19:56:25 -0800

    added tiny card stuff

---
+++ card.gd
@@ -0,0 +1,6 @@
+extends Resource
+class_name Card
+
+var name : String = ""
+
+var type : int = 0b10011
---
+++ card_gui.gd
@@ -0,0 +1,6 @@
+extends Control
+
+var stored_card : Card
+
+func open_interaction_menu() -> void:
+	pass # Opens a menu with the abilities, passives, attack/move options, etc etc.
