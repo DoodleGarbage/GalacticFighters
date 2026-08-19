b8223e14236d3fb29dc53f79e8e6d3d136f9ba04
BloshBlu12 <christlopez2009@gmail.com>
2026-08-18 19:35:44 -0500

    Small Change
    - Added Trooper
    - Added Marksman
    - Added Sandvich
    
    - Nerfed Fire DMG from 4 -> 2

---
+++ Data/Vanilla/PowerScripts/Sandvich.gd
@@ -0,0 +1,18 @@
+extends PowerScript
+
+@export var heal_amnt : int = 8
+
+## Triggered when the associated ability or move triggers a targeting request.
+func _interaction(_targets:Array[Card_GUI]) -> void:
+	for target in _targets:
+		if not is_instance_valid(target):
+			continue
+		target.heal(ability, heal_amnt)
+
+## Called at the start of the game turn
+func _turnstart() -> void:
+	pass
+
+## Called when the turn ends.
+func _turnend() -> void:
+	pass
