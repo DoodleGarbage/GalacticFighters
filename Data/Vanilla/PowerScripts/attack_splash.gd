307af8e1b86475410de50e022f08730b6ff78529
DoodleGarbage <redstone48rocks@gmail.com>
2026-08-22 16:37:38 -0700

    implement tuber splash damage

---
+++ Data/Vanilla/PowerScripts/attack_splash.gd
@@ -0,0 +1,23 @@
+extends PowerScript
+
+@export var radius : int = 0
+
+## Triggered when the associated ability or move triggers a targeting request.
+func _interaction(_targets:Array[Card_GUI]) -> void:
+	if source == null:
+		return
+	for target in _targets:
+		if not is_instance_valid(target):
+			continue
+		var splash_targets : Array[Card_GUI] = []
+		splash_targets.assign(get_splash(target.manager, target.manager_position, radius))
+		for splash in splash_targets:
+			splash.damage(ability, source.get_attack(), source.get_armor_pierce())
+
+## Called at the start of the game turn
+func _turnstart() -> void:
+	pass
+
+## Called when the turn ends.
+func _turnend() -> void:
+	pass
